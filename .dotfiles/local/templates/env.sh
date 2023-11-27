9781f89f13d38313bbe86272b4f7dc69b365329f
Andy Barron <andybarron@protonmail.com>
2023-11-26 17:43:50 -0800

    empty local env by default

--- .dotfiles/local/templates/env.sh
+++ .dotfiles/local/templates/env.sh
@@ -3,6 +3,6 @@
 
 # system-local enviornment (i.e. not synced across machines)
 
-export RC__ENABLE_TMUX=1
-export RC__TMUX_STATUS_BG='#006699'
-export RC__TMUX_STATUS_FG='white'
+# export RC__ENABLE_TMUX=1
+# export RC__TMUX_STATUS_BG='#006699'
+# export RC__TMUX_STATUS_FG='white'
