873e6bc5fb168e26c538ae8b9ff8ccbde44d912a
Andy Barron <andybarron@protonmail.com>
2024-09-18 16:40:41 -0700

    allow local overrides for spaceship prompt

--- packages/spaceship/.config/spaceship/spaceship.zsh
+++ packages/spaceship/.config/spaceship/spaceship.zsh
@@ -1,3 +1,8 @@
 SPACESHIP_PROMPT_PREFIXES_SHOW=false
 SPACESHIP_GIT_STATUS_PREFIX=' '
 SPACESHIP_GIT_STATUS_SUFFIX=
+
+# local env overrides
+spaceship_local_override="$HOME/.config/spaceship/spaceship.local.zsh"
+[ -f "$spaceship_local_override" ] && source "$spaceship_local_override"
+unset spaceship_local_override
