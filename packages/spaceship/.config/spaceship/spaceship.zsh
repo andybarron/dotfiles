b5681346d070d7a8e34a7cb7339daa166561e852
Andy Barron <andybarron@protonmail.com>
2025-02-05 12:52:18 -0800

    update repos, spaceship async fix

--- packages/spaceship/.config/spaceship/spaceship.zsh
+++ packages/spaceship/.config/spaceship/spaceship.zsh
@@ -2,6 +2,10 @@ SPACESHIP_PROMPT_PREFIXES_SHOW=false
 SPACESHIP_GIT_STATUS_PREFIX=' '
 SPACESHIP_GIT_STATUS_SUFFIX=
 
+# temporary workaround
+# https://github.com/spaceship-prompt/spaceship-prompt/issues/1193
+SPACESHIP_ASYNC_SYMBOL=''
+
 # local env overrides
 spaceship_local_override="$HOME/.config/spaceship/spaceship.local.zsh"
 [ -f "$spaceship_local_override" ] && source "$spaceship_local_override"
