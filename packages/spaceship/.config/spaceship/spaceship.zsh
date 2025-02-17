80827c6bd9e95651ffb9f07e9ffbe2a517f55a5c
Andy Barron <andybarron@protonmail.com>
2025-02-16 22:10:13 -0800

    re-enable async indicator

--- packages/spaceship/.config/spaceship/spaceship.zsh
+++ packages/spaceship/.config/spaceship/spaceship.zsh
@@ -1,10 +1,4 @@
 SPACESHIP_PROMPT_PREFIXES_SHOW=false
-SPACESHIP_GIT_STATUS_PREFIX=' '
-SPACESHIP_GIT_STATUS_SUFFIX=
-
-# temporary workaround
-# https://github.com/spaceship-prompt/spaceship-prompt/issues/1193
-SPACESHIP_ASYNC_SYMBOL=''
 
 # local env overrides
 spaceship_local_override="$HOME/.config/spaceship/spaceship.local.zsh"
