4bfa87a1ee3080da2ea941512f10cfb4f0651cd0
Andy Barron <andybarron@protonmail.com>
2025-08-14 09:55:11 -0700

    fix netskope bs

--- packages/zsh/.config/zsh/.zshrc.zatannamac.zsh
+++ packages/zsh/.config/zsh/.zshrc.zatannamac.zsh
@@ -1,1 +1,1 @@
-# export NODE_EXTRA_CA_CERTS="/Library/Application Support/Netskope/STAgent/data/nscacert.pem"
+export NODE_EXTRA_CA_CERTS="/Library/Application Support/Netskope/STAgent/data/nscacert.pem"
