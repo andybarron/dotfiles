92baeab4522ed815e52372236d18cb329f206b38
Andy Barron <andybarron@protonmail.com>
2026-02-25 02:43:56 -0800

    spaceship async fix

--- packages/spaceship/.config/spaceship/spaceship.zsh
+++ packages/spaceship/.config/spaceship/spaceship.zsh
@@ -1,5 +1,8 @@
 SPACESHIP_PROMPT_PREFIXES_SHOW=false
 
+# https://github.com/spaceship-prompt/spaceship-prompt/issues/1193
+SPACESHIP_ASYNC_SHOW=false
+
 function {
   spaceship_local_override="$HOME/.config/spaceship/spaceship.$(hostname).zsh"
   [ -f "$spaceship_local_override" ] && source "$spaceship_local_override"
